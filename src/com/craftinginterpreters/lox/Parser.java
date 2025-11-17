9bc575bec5f8d8f326d80db230713a80541aa881
Isabel Araujo <166257491+isabel1993araujo@users.noreply.github.com>
2025-11-17 17:22:30 -0300

    Add Parser class for token parsing

---
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,13 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+public class Parser {
+
+    private final List<Token> tokens;
+    private int current = 0;
+
+    public Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+}
