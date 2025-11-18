92f1f892a7190466f86f8eb0f89c7ba163ba5570
kleitonlinneker <kleiton.linneker@discente.ufma.br>
2025-11-17 23:04:03 -0300

    Atualização classe LOX e revisao classe Parser

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -2,8 +2,9 @@ package com.craftinginterpreters.lox;
 
 import java.util.List;
 
-class Parser {
+import static com.craftinginterpreters.lox.TokenType.*;
 
+class Parser {
     private static class ParseError extends RuntimeException {}
 
     private final List<Token> tokens;
@@ -13,6 +14,7 @@ class Parser {
         this.tokens = tokens;
     }
 
+
     Expr parse() {
         try {
             return expression();
@@ -20,11 +22,30 @@ class Parser {
             return null;
         }
     }
-private Expr comparison() {
+
+
+    private Expr expression() {
+        return equality();
+    }
+
+
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+
+    private Expr comparison() {
         Expr expr = term();
 
-        while (match(TokenType.GREATER, TokenType.GREATER_EQUAL,
-                     TokenType.LESS, TokenType.LESS_EQUAL)) {
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
             Token operator = previous();
             Expr right = term();
             expr = new Expr.Binary(expr, operator, right);
@@ -33,10 +54,11 @@ private Expr comparison() {
         return expr;
     }
 
+
     private Expr term() {
         Expr expr = factor();
 
-        while (match(TokenType.MINUS, TokenType.PLUS)) {
+        while (match(MINUS, PLUS)) {
             Token operator = previous();
             Expr right = factor();
             expr = new Expr.Binary(expr, operator, right);
@@ -45,10 +67,11 @@ private Expr comparison() {
         return expr;
     }
 
+
     private Expr factor() {
         Expr expr = unary();
 
-        while (match(TokenType.SLASH, TokenType.STAR)) {
+        while (match(SLASH, STAR)) {
             Token operator = previous();
             Expr right = unary();
             expr = new Expr.Binary(expr, operator, right);
@@ -56,8 +79,10 @@ private Expr comparison() {
 
         return expr;
     }
-private Expr unary() {
-        if (match(TokenType.BANG, TokenType.MINUS)) {
+
+
+    private Expr unary() {
+        if (match(BANG, MINUS)) {
             Token operator = previous();
             Expr right = unary();
             return new Expr.Unary(operator, right);
@@ -66,45 +91,59 @@ private Expr unary() {
         return primary();
     }
 
-    private Expr primary() {
-        if (match(TokenType.FALSE)) return new Expr.Literal(false);
-        if (match(TokenType.TRUE))  return new Expr.Literal(true);
-        if (match(TokenType.NIL))   return new Expr.Literal(null);
 
-        if (match(TokenType.NUMBER, TokenType.STRING)) {
+    private Expr primary() {
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+
+        if (match(NUMBER, STRING)) {
             return new Expr.Literal(previous().literal);
         }
 
-        if (match(TokenType.LEFT_PAREN)) {
+        if (match(LEFT_PAREN)) {
             Expr expr = expression();
-            consume(TokenType.RIGHT_PAREN, "Esperado ')' após expressão.");
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
             return new Expr.Grouping(expr);
         }
 
-        throw error(peek(), "Esperado expressão.");
+        throw error(peek(), "Expect expression.");
     }
-  private boolean match(TokenType... types) {
+
+
+    private boolean match(TokenType... types) {
         for (TokenType type : types) {
             if (check(type)) {
                 advance();
                 return true;
             }
         }
+
         return false;
     }
+
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) return advance();
+
+        throw error(peek(), message);
+    }
+
 
     private boolean check(TokenType type) {
         if (isAtEnd()) return false;
         return peek().type == type;
     }
 
+
     private Token advance() {
         if (!isAtEnd()) current++;
         return previous();
     }
 
+
     private boolean isAtEnd() {
-        return peek().type == TokenType.EOF;
+        return peek().type == EOF;
     }
 
     private Token peek() {
@@ -115,23 +154,18 @@ private Expr unary() {
         return tokens.get(current - 1);
     }
 
-    private Token consume(TokenType type, String message) {
-        if (check(type)) return advance();
-
-        throw error(peek(), message);
-    }
 
     private ParseError error(Token token, String message) {
         Lox.error(token, message);
         return new ParseError();
     }
 
-    // Usado se quiser sincronizar após erro, em estágios futuros (declarações)
+
     private void synchronize() {
         advance();
 
         while (!isAtEnd()) {
-            if (previous().type == TokenType.SEMICOLON) return;
+            if (previous().type == SEMICOLON) return;
 
             switch (peek().type) {
                 case CLASS:
@@ -148,4 +182,5 @@ private Expr unary() {
             advance();
         }
     }
+
 }
