4799f8b34b0cf62b33c7b86799e1cb1d3d7e592e
kleitonlinneker <kleiton.linneker@discente.ufma.br>
2025-11-23 11:40:17 -0300

    Atualização Classe Interpreter e classe Lox

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -48,6 +48,20 @@ class Interpreter implements Expr.Visitor<Object> {
         return a.equals(b);
     }
 
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
+    }
+
     @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
@@ -104,6 +118,15 @@ class Interpreter implements Expr.Visitor<Object> {
 
 
 
+    }
+
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
     }
 
 
